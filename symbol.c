a89ca7808ccab4ab0b4783439c87c145f0377838
fadi <fadi.afani2@gmail.com>
2024-03-06 03:56:37 +0200

    fixed compilation issues

--- symbol.c
+++ symbol.c
@@ -8,7 +8,7 @@ static inline float get_load_factor(SymbolTable* table) {
 Symbol* lookup(SymbolTable* table, char* key) {
     int i = strlen(key) % (table->entries.size);
     for (size_t j = i; j < table->entries.size; i++) {
-        Symbol* sym = INDEX_VECTOR(table->entries, Symbol**, i);
+        Symbol* sym = INDEX_VECTOR(table->entries, Symbol*, i);
         if (sym == NULL) return NULL;
 
         if (strcmp(sym->key, key) == 0) {
@@ -24,11 +24,11 @@ void insert(SymbolTable* table, char* key, Symbol* value) {
     if (get_load_factor(table) >= LOAD_FACTOR) {
         // refactor
         Vector* entries = &(table->entries);
-        REALLOCATE(entries, table->entries.capacity, Symbol*);
+        REALLOCATE(entries->arr, table->entries.capacity, Symbol);
         table->entries.capacity *= 2;
     }
     for (size_t j = i; j < table->entries.size; i++) {
-        Symbol* sym = INDEX_VECTOR(table->entries, Symbol**, i);
+        Symbol* sym = INDEX_VECTOR(table->entries, Symbol*, i);
         if (sym == NULL || strcmp(sym->key, key) == 0) {
             APPEND(table->entries, value, Symbol*);
             break;
